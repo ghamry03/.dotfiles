42654e55f1cb788faaf8c9bc62a3c2680ee01f0d
Omar <omarelghamry@icloud.com>
2024-04-08 22:45:03 +0400

    added tmux navg to nvim

--- .config/nvim-dev/lua/custom/plugins/init.lua
+++ .config/nvim-dev/lua/custom/plugins/init.lua
@@ -3,19 +3,4 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-        'christoomey/vim-tmux-navigator',
-        cmd = {
-            "TmuxNavigateLeft",
-            "TmuxNavigateDown",
-            "TmuxNavigateUp",
-            "TmuxNavigateRight",
-            "TmuxNavigatePrevious",
-        },
-        keys = {
-            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<CR>" },
-            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<CR>" },
-            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<CR>" },
-            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<CR>" },
-            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<CR>" },
-        },
 }
